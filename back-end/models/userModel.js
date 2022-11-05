9d3374b32d2c77a78c7447a4a582a88180a798b7
chris434 <christopherpearce589@gmail.com>
2022-11-05 07:26:29 +0000

    user model refactor

--- back-end/models/userModel.js
+++ back-end/models/userModel.js
@@ -1,11 +1,17 @@
+import { profile } from 'console'
 import mongoose from 'mongoose'
 import uniqueValidator from 'mongoose-unique-validator'
 
 const userSchema = new mongoose.Schema({
     email: { type: 'string', unique: true, required: true },
-    name: { type: 'string', maxLength: 25, required: true },
+    username: { type: 'string', unique: true, required: true },
     password: { type: 'string', minLength: 8, required: true },
-    SignedUpDate: { type: 'string', default: Date.now() }
+    SignedUpDate: { type: 'string', default: Date.now() },
+
+    profile: {
+        photoURL: { type: 'string', default: '' },
+        privateProfile: { type: 'boolean', default: true }
+    }
 })
 userSchema.plugin(uniqueValidator)
 export const User = mongoose.model('user', userSchema)
